d5bd5ea435ef5bcac4191cda9864e7c997bdebc0
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-05-02 23:24:28 +0530

    fixed .env

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
-
-
-import {connectDB} from"./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 
-const PORT= process.env.PORT;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use("/api/auth", authRoutes);
-app.listen(PORT,()=>{
-    console.log("server is running on PORT:" + PORT);
+
+// Test route
+app.get("/ping", (req, res) => {
+    res.status(200).send("Server is running 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT: " + PORT);
     connectDB();
 });
